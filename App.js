4fc3255827b036df3a3c7905bedd24882d6bc093
Anil Duri <anilduri.a@gmail.com>
2022-04-01 12:42:48 +0300

    edited navigation configuration

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import { Ionicons } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 
+import { GlobalStyles } from './constants/styles'
 import AllExpenses from './screens/AllExpenses'
 import ManageExpense from './screens/ManageExpense'
 import RecentExpenses from './screens/RecentExpenses'
@@ -12,9 +14,29 @@ const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function ExpensesOverview() {
-  return <BottomTabs.Navigator screenOptions={{}}>
-    <BottomTabs.Screen name='RecentExpenses' component={RecentExpenses}></BottomTabs.Screen>
-    <BottomTabs.Screen name='AllExpenses' component={AllExpenses}></BottomTabs.Screen>
+  return <BottomTabs.Navigator screenOptions={{
+    headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+    headerTintColor: 'white',
+    tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+    tabBarActiveTintColor: GlobalStyles.colors.accent500
+  }}>
+    <BottomTabs.Screen
+      name='RecentExpenses'
+      component={RecentExpenses}
+      options={{
+        title: 'Recent Expenses',
+        tabBarLabel: 'Recent',
+        tabBarIcon: ({ color, size }) => (<Ionicons name='hourglass' size={size} color={color} />)
+      }}></BottomTabs.Screen>
+    <BottomTabs.Screen
+      name='AllExpenses'
+      component={AllExpenses}
+      options={{
+        title: 'All Expenses',
+        tabBarLabel: 'All Expenses',
+        tabBarIcon: ({ color, size }) => (<Ionicons name='calendar' size={size} color={color} />)
+      }}
+    ></BottomTabs.Screen>
   </BottomTabs.Navigator>
 }
 
